b41613fda26705a7bf11b26d96ae3fe7355b7a96
Dave Syer <dsyer@pivotal.io>
2015-01-29 17:42:26 +0000

    Adjust to new @FeignCLient semantics

--- rest-microservices-store/src/test/java/example/stores/TestStoreApp.java
+++ rest-microservices-store/src/test/java/example/stores/TestStoreApp.java
@@ -1,19 +1,19 @@
 package example.stores;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.FeignConfiguration;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Spencer Gibb
@@ -21,29 +21,28 @@ import java.util.Map;
 @Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
-public class TestStoreApp extends FeignConfiguration {
+@EnableFeignClients
+public class TestStoreApp {
 
-    @Bean
-    public StoreClient storeClient() {
-        return feign().target(StoreClient.class, "http://localhost:8081");
-    }
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new SpringApplicationBuilder()
+				.web(false).sources(TestStoreApp.class).run(args);
+		StoreClient client = context.getBean(StoreClient.class);
+		List<Map<String, ?>> stores = client.getStores();
+		if (!stores.isEmpty()) {
+			Map<String, ?> data = stores.get(0);
+			Store store = client.getStore(data.get("id").toString(), "myval");
+			System.out.println(store);
+		}
+	}
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = new SpringApplicationBuilder().web(false).sources(TestStoreApp.class).run(args);
-        StoreClient client = context.getBean(StoreClient.class);
-        List stores = client.getStores();
-        if (!stores.isEmpty()) {
-            Map data = (Map)stores.get(0);
-            Store store = client.getStore(data.get("id").toString(), "myval");
-            System.out.println(store);
-        }
-    }
+	@FeignClient(url = "http://localhost:8081")
+	public interface StoreClient {
+		@RequestMapping(method = RequestMethod.GET, value = "/simple/stores")
+		List<Map<String, ?>> getStores();
 
-    public interface StoreClient {
-        @RequestMapping(method = RequestMethod.GET, value = "/simple/stores")
-        List getStores();
-
-        @RequestMapping(method = RequestMethod.GET, value = "/stores/{storeId}", produces = "application/hal+json")
-        Store getStore(@PathVariable("storeId") String storeId, @RequestParam("myparam") String myparam);
-    }
+		@RequestMapping(method = RequestMethod.GET, value = "/stores/{storeId}", produces = "application/hal+json")
+		Store getStore(@PathVariable("storeId") String storeId,
+				@RequestParam("myparam") String myparam);
+	}
 }
