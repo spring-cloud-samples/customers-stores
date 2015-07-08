1cfb2b1c6baacae0044c89c5ee2741b7471ff7ab
Andy Wilkinson <awilkinson@pivotal.io>
2015-07-08 07:56:15 +0100

    Remove RepositoryRestMvcConfiguration subclasses

--- rest-microservices-store/src/main/java/example/stores/StoreApp.java
+++ rest-microservices-store/src/main/java/example/stores/StoreApp.java
@@ -17,16 +17,16 @@ package example.stores;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -39,13 +39,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 @ComponentScan
 @EnableDiscoveryClient
-@Import(RepositoryRestMvcConfiguration.class)
-public class StoreApp extends RepositoryRestMvcConfiguration {
+public class StoreApp {
 
-    @Override
-    protected void configureRepositoryRestConfiguration( RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Store.class);
-    }
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+
+	@PostConstruct
+	public void exposeIds() {
+		this.repositoryRestConfiguration.exposeIdsFor(Store.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApp.class, args);
