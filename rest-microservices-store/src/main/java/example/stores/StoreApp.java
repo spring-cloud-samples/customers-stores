486fc3bb8b15d0a88688aab028a2383a8f851ce7
Dave Syer <dsyer@pivotal.io>
2015-07-22 16:33:54 +0100

    Workaround for bug in connectors (or buildpack)
    Spring Cloud Connectors (including the one bundled with the
    autoconfiguration) has a bug that trips with Spring Data Mongodb
    1.8.0 (so in Spring Boot 1.3). We can work around it by fixing the
    app to switch off the connectors for Mongo and wire in the URI
    via vcap.services (adding the bean of type Cloud does that).

--- rest-microservices-store/src/main/java/example/stores/StoreApp.java
+++ rest-microservices-store/src/main/java/example/stores/StoreApp.java
@@ -22,8 +22,13 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -66,4 +71,16 @@ public class StoreApp {
             return all.getContent();
         }
     }
+    
+    @Configuration
+    @Profile("cloud")
+    protected static class CloudFoundryConfiguration {
+    	
+    	@Bean
+    	  public Cloud cloud() {
+    	    return new CloudFactory().getCloud();
+    	  }
+
+    }
+
 }
